a719bcbb710f6473e82cd74a7d70f4453cd9fa2e
Your Name <you@example.com>
2023-02-28 02:22:35 +0500

    Update requirements.txt

--- setup.py
+++ setup.py
@@ -6,12 +6,11 @@ from utils import run
 
 def setup():
     run(['pip', 'install', '-r requirements.txt'])
-    if not os.path.exists("pretrained_models"):
-        os.mkdir("pretrained_models")
+    if not os.path.exists("workhorse/pretrained_models"):
+        os.mkdir("workhorse/pretrained_models")
 
-    gdown.download_folder(url='https://drive.google.com/drive/u/1/folders/1q8110-qp225asX3DQvZnfLfJPkCHmDpy', output='pretrained_models')
+    gdown.download_folder(url='https://drive.google.com/drive/u/1/folders/1q8110-qp225asX3DQvZnfLfJPkCHmDpy', output='workhorse/pretrained_models')
 
-    run(['python', 'setup.py', 'develop'], 'GFPGAN')
-    urllib.request.urlretrieve('https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth', 'GFPGAN/experiments/pretrained_models/GFPGANv1.4.pth')
+    urllib.request.urlretrieve('https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth', 'workhorse/GFPGAN/experiments/pretrained_models/GFPGANv1.4.pth')
 
 setup()
